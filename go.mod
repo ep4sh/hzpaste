8df6a4bddfdd287a2bdee8cca151289031a76b09
Pasha Radchenko <ep4sh2k@mail.com>
2021-06-20 23:26:00 +0300

    Add List handlers

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/ep4sh/hastepaste
 go 1.16
 
 require (
-	github.com/gin-gonic/gin v1.7.2 // indirect
+	github.com/gin-gonic/gin v1.7.2
 	github.com/go-playground/validator/v10 v10.6.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
@@ -11,6 +11,7 @@ require (
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/ugorji/go v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
 	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"log"
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 )
 
 type Paste struct {
@@ -18,11 +19,16 @@ type Pastes struct {
 	PasteList []Paste
 }
 
+var (
+	ErrNotFound = errors.New("Paste not found")
+)
+
 func main() {
 	route := gin.Default()
 
 	pastes := Pastes{}
 
+	route.GET("/pastes", pastes.ListPastes)
 	route.POST("/pastes", pastes.AddPaste)
 	route.Run(":8888")
 }
@@ -36,14 +42,32 @@ func (ps *Pastes) AddPaste(c *gin.Context) {
 	}
 	all, err := ps.Add(np)
 	if err != nil {
-		log.Fatalf("%+v\n", err)
+		c.JSON(http.StatusNoContent, gin.H{"pastes": all})
 	}
 
-	c.JSON(200, gin.H{"pastes": all})
+	c.JSON(http.StatusOK, gin.H{"pastes": all})
+}
+
+func (ps *Pastes) ListPastes(c *gin.Context) {
+	all, err := ps.List()
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"pastes": all})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"pastes": all})
 }
 
 // -------------------- HANDLERS
 func (ps *Pastes) Add(np Paste) (*Paste, error) {
 	ps.PasteList = append(ps.PasteList, np)
 	return &np, nil
+}
+
+func (ps *Pastes) List() ([]Paste, error) {
+	var EmptyPastes []Paste
+	if ps.PasteList == nil {
+		return EmptyPastes, ErrNotFound
+	}
+	return ps.PasteList, nil
 }
